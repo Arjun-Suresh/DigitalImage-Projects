51e6336d4e3e1e427748b390399594e386f99cfa
Live session user <arjun.sureshn2000@gmail.com>
2017-10-07 23:38:58 +0000

    INitial changes

---
+++ pr03.cpp
@@ -0,0 +1,512 @@
+// =============================================================================
+// VIZA654/CSCE646 at Texas A&M University
+// Homework 3
+// Generating convex quadrilateral (and other convex figures), star, function field, blobby and shaded circle
+// with antialiazing
+// For convex quadrilateral and star, input is taken from txt file on the same folder
+// Data format in the txt file:
+// x0 y0
+// x1 y1
+// x2 y2
+// and so on
+// =============================================================================
+
+#include <cstdlib>
+#include <iostream>
+#include <GL/glut.h>
+#include <math.h>
+#include <fstream>
+#include <cassert>
+#include <sstream>
+#include <cstring>
+#include <malloc.h>
+
+
+#define WIDTHVALUEID 1
+#define HEIGHTVALUEID 2
+#define MAXCOLORVALUEID 3
+#define COLORFOREGROUND 0
+#define COLORBACKGROUND 1
+
+using namespace std;
+// =============================================================================
+// These variables will store the input ppm image's width, height, and color
+// =============================================================================
+int width, height, maxColorValue, magicNo;
+unsigned char *pixmapOrig, *pixmapComputed;
+
+//Function to resize ppm file buffer array
+unsigned char* resizeArray(unsigned char* oldArray, long int oldSize, long int& newSize) 
+{
+    newSize = oldSize * 2;
+    unsigned char* newArray = new unsigned char[newSize];
+    std::memcpy( newArray, oldArray, oldSize * sizeof(unsigned char) );
+    return newArray;
+}
+
+//Set red, green and blue in the new pixMap array to be written to the ppm file
+void setPixelColor(int y, int x, int red, int green, int blue)
+{
+  int i = (y * width + x) * 3; 
+  pixmapComputed[i++] = red;
+  pixmapComputed[i++] = green;
+  pixmapComputed[i] = blue;
+}
+
+void setPixelColorOrig(long int &val, long int& index, unsigned char* fileBuffer)
+{
+  pixmapOrig[val++]=fileBuffer[index++];
+}
+// =============================================================================
+// OpenGL Display and Mouse Processing Functions.
+//
+// You can read up on OpenGL and modify these functions, as well as the commands
+// in main(), to perform more sophisticated display or GUI behavior. This code
+// will service the bare minimum display needs for most assignments.
+// =============================================================================
+static void windowResize(int w, int h)
+{   
+  glViewport(0, 0, w, h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0,(w/2),0,(h/2),0,1); 
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity() ;
+}
+static void windowDisplay(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+  glRasterPos2i(0,0);
+  glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+  glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, pixmapComputed);
+  glFlush();
+}
+static void processMouse(int button, int state, int x, int y)
+{
+  if(state == GLUT_UP)
+  exit(0);               // Exit on mouse click.
+}
+static void init(void)
+{
+  glClearColor(1,1,1,1); // Set background color.
+}
+
+
+
+
+//****************************************************************************************************************************
+//**********************Functions to read data from ppm file******************************************************************
+//****************************************************************************************************************************
+void parseCommentLine(long int& index, unsigned char* fileBuffer, long int numOfCharacters)
+{
+  while(index<numOfCharacters && fileBuffer[index]!='\n')
+  index++;
+}
+
+bool parseMagicNumber(long int& index, unsigned char* fileBuffer, long int numOfCharacters, int& magicNumberParsed)
+{
+  while(index<numOfCharacters)
+  {
+    if(fileBuffer[index]=='#')
+    {
+      parseCommentLine(index, fileBuffer, numOfCharacters);
+      break;
+    }
+    if(fileBuffer[index]=='P' || fileBuffer[index]=='p')
+    {
+      index++;
+      if(fileBuffer[index]=='1' || fileBuffer[index]=='2'|| fileBuffer[index]=='3' || fileBuffer[index]=='4' || fileBuffer[index]=='5' || fileBuffer[index]=='6')
+      {
+        magicNumberParsed=1;
+	magicNo=fileBuffer[index]-48;
+        break;
+      }
+      else 
+        return false;
+    }
+    if(isspace(fileBuffer[index]) && fileBuffer[index]!='\n')
+      index++;
+    else
+      return false;
+  }
+  if(magicNumberParsed)
+  {
+    index++;
+    if(index<numOfCharacters && !(isspace(fileBuffer[index]) || fileBuffer[index]=='#'))
+      return false;
+    else
+    {
+      if(fileBuffer[index]=='#')
+      {
+        parseCommentLine(index, fileBuffer, numOfCharacters);
+      }
+    }
+        
+  }
+  return true;
+}
+
+bool parseValue(long int& index, unsigned char* fileBuffer, long int numOfCharacters, int& parseValue, int valueId)
+{
+  int value;
+  while(index<numOfCharacters)
+  {
+    if(fileBuffer[index]=='#')
+    {
+      parseCommentLine(index, fileBuffer, numOfCharacters);
+      break;
+    }
+    if(isdigit(fileBuffer[index]))
+    {
+      char valueString[20];
+      int k=0;
+      valueString[k++]=fileBuffer[index++];
+
+      while(isdigit(fileBuffer[index]) && index<numOfCharacters)
+        valueString[k++]=fileBuffer[index++];
+      valueString[k]='\0';
+      value = atoi(valueString);
+      parseValue=1;
+      break;
+    }
+    if(isspace(fileBuffer[index]))
+      index++;
+    else
+      return false;
+  }
+  if(parseValue)
+  {
+    if(index<numOfCharacters && !((isspace(fileBuffer[index]) && valueId != MAXCOLORVALUEID) || (valueId == MAXCOLORVALUEID && fileBuffer[index] == '\n') || fileBuffer[index]=='#'))
+      return false;
+    else
+    {
+      if(fileBuffer[index]=='#')
+      {
+        parseCommentLine(index, fileBuffer, numOfCharacters);
+      }
+    }        
+  }
+  switch(valueId)
+  {
+    case WIDTHVALUEID:
+      width=value;
+      break;
+    case HEIGHTVALUEID:
+      height=value;
+      break;
+    case MAXCOLORVALUEID:
+      maxColorValue=value;
+      break;
+  }
+  return true;
+}
+
+bool fillPixelsBin(long int& index, unsigned char* fileBuffer, long int numOfCharacters)
+{
+  long int rowVal=0,colVal=0;
+  while(rowVal<height)
+  {
+    long int val=(((height-rowVal-1)*width)+colVal)*3;
+    for(int i=0;i<3;i++)
+    	setPixelColorOrig(val, index, fileBuffer);
+    colVal=(colVal+1)%width;
+    if(!colVal)
+      rowVal++;
+  }
+  return true;
+}
+
+bool readPPMFile(char* filePath)
+{
+  std::fstream ppmFile;
+  std::ifstream checkFile(filePath);
+  long int numOfCharacters=0;
+  int magicNumberParsed=0, widthParsed=0, heightParsed=0, maxColorValueParsed=0;
+  if(!checkFile.good())
+  {
+    cout<<"Filename not found\n";
+    return false;
+  }
+  ppmFile.open (filePath, std::ios::in|std::ios::binary);
+  ppmFile.seekg(0,std::ios::end);
+  ppmFile.seekg(0,std::ios::end);
+  numOfCharacters = ppmFile.tellg();
+  ppmFile.seekg(0,std::ios::beg);
+  unsigned char* fileBuffer = new unsigned char[numOfCharacters+1];
+  ppmFile.read((char *)fileBuffer, numOfCharacters);
+  fileBuffer[numOfCharacters]='\0';
+  ppmFile.close();
+  for(long int index=0;index<numOfCharacters;index++)
+  {
+    if(fileBuffer[index]=='#')
+    {
+      parseCommentLine(index, fileBuffer, numOfCharacters);
+      continue;
+    }
+    if(!magicNumberParsed)
+    {
+      if(!parseMagicNumber(index, fileBuffer, numOfCharacters, magicNumberParsed))
+      {
+        return false;
+      }
+        
+      continue;
+    }
+    if(magicNumberParsed && !widthParsed)
+    {
+      if(!parseValue(index, fileBuffer, numOfCharacters, widthParsed, WIDTHVALUEID))
+      {
+        return false;
+      }
+      continue;
+    }
+    if(magicNumberParsed && widthParsed && !heightParsed)
+    {
+      if(!parseValue(index, fileBuffer, numOfCharacters, heightParsed, HEIGHTVALUEID))
+      {
+        return false;
+      }
+      continue;
+    }
+    if(magicNumberParsed && widthParsed && heightParsed && !maxColorValueParsed)
+    {
+      if(!parseValue(index, fileBuffer, numOfCharacters, maxColorValueParsed, MAXCOLORVALUEID))
+      {
+        return false;
+      }
+      continue;
+    }
+    if(magicNumberParsed && widthParsed && heightParsed && maxColorValueParsed)
+    {
+      pixmapOrig = new unsigned char[width*height*3];
+      if(!(fillPixelsBin(index, fileBuffer, numOfCharacters)))
+        return false;
+      return true;
+    }
+  }
+  delete[] fileBuffer;
+  return true;
+}
+
+
+
+
+
+//************************************************************************************************************
+//**********************Functions to save from pixelMap to ppm file*******************************************
+//************************************************************************************************************
+void fillCharacters(unsigned char* fileBuffer, long int& index, char* data)
+{
+  for(int i=0;i<strlen(data);i++)
+    fileBuffer[index++]=data[i];
+}
+
+void writeToFile(unsigned char* fileBuffer, long int numberOfCharacters, fstream& ppmFile)
+{  
+  for(long int i=0; i< numberOfCharacters; i++)
+  {
+    ppmFile << fileBuffer[i];
+  }
+}
+
+void writeHeader(unsigned char* fileBuffer, long int& index)
+{
+  maxColorValue=255;
+  char widthString[5], heightString[5], maxColorString[5];
+  sprintf(widthString, "%d", width);
+  sprintf(heightString, "%d", height);
+  sprintf(maxColorString, "%d", maxColorValue);
+  char magicNumberString[3];
+  strcpy(magicNumberString, "P6");
+  fillCharacters(fileBuffer, index, magicNumberString);
+  fileBuffer[index++]='\n';
+  fillCharacters(fileBuffer, index, widthString);
+  fileBuffer[index++]=' ';
+  fillCharacters(fileBuffer, index, heightString);
+  fileBuffer[index++]='\n';
+  fillCharacters(fileBuffer, index, maxColorString);
+  fileBuffer[index++]='\n';  
+}
+
+
+unsigned char* writePixelBufferToFileBuffer(unsigned char* fileBuffer, long int& index, long int origSize)
+{
+  int charCount=0;
+  for(int i=0;i<height;i++)
+  {
+    for(int j=0;j<width;j++)
+    {
+      if(index >= origSize-50)
+      {
+        unsigned char* tempBuffer = resizeArray(fileBuffer, origSize, origSize);
+        delete[] fileBuffer;
+        fileBuffer=tempBuffer;
+      }
+      int k=((height-i-1)*width+j)*3;
+      for(int x=0;x<3;x++)
+      fileBuffer[index++]=pixmapComputed[k++];
+    }
+  }
+  return fileBuffer;
+}
+
+void generatePPMFile(int option)
+{
+  std::fstream ppmFile;
+  char fileName[50];
+  switch(option)
+  {
+    case 1: 
+      strcpy(fileName,"outputLinear.ppm");
+      break;
+    case 2: 
+      strcpy(fileName,"outputCubic.ppm");
+      break;
+  }
+   
+  ppmFile.open(fileName,std::fstream::out);
+  long int index=0;
+  unsigned char* fileBuffer = new unsigned char[10000];
+  writeHeader(fileBuffer, index);
+  fileBuffer=writePixelBufferToFileBuffer(fileBuffer, index, 10000);
+  writeToFile(fileBuffer, index, ppmFile);
+  ppmFile.close();
+}
+
+
+
+//***********************************************************************************************
+//****************************Read data from txt file to get the curve points********************
+//***********************************************************************************************
+void readData(char* filePath, int* rArray, int& num)
+{
+  std::fstream inputFile;
+  int x,y,count=0;
+  char c, cArray[10];
+  int k=0;  
+  cout<<"Reading data from "<<filePath<<"\n";
+  inputFile.open(filePath, std::fstream::in);
+  while(inputFile.get(c))
+  {
+    if(c == ' ' || c == '\n')
+    {
+      if(k!=0)
+      {
+        cArray[k]='\0';
+        k=0;
+        string num(cArray);
+        istringstream(num) >> x;
+        rArray[count++]=x;
+      }
+    }
+    else
+    {
+      cArray[k++]=c;
+    }
+  }
+  num=count;
+}
+
+
+
+
+
+//************************************************************************************************
+//*************************************Image Manipulation functions*******************************
+//************************************************************************************************
+int findInterval(float color, float* x, int n)
+{
+  
+}
+
+int interpolateLinear(int color, float* x, float* y, int n)
+{
+  float colorVal = (float)color/255;  
+  int interval = findInterval(colorVal,x,n);
+  if(interval==n)
+    return (int)y[n];
+  float tVal = (colorVal-x[interval])/(x[interval+1]-x[interval]);
+  float output = (y[interval]*(1-tVal))+(y[interval+1]*t);
+  cout<<output<<endl;
+  if (output>255)
+  {
+    cout<<output<<endl;
+    char c;
+    cin<<c;
+  }
+  return (int)output;
+}
+
+void colorManipulate(int* rArray, int n)
+{
+  int red,green,blue;
+  float x[1000],y[1000];
+  formXYPoints(x,y,rArray,n);
+  for (int y=0;y<height;y++)
+  {
+    for(int x=0;x<width;x++)
+    {      
+      int i = (y * width + x) * 3;
+      red=pixmapOrig[i++];
+      red=interpolate(red,x,y,n);
+      green=pixmapOrig[i++];
+      blue=pixmapOrig[i];
+      setPixelColor(y,x,red,green,blue);
+    }
+  }
+}
+
+
+
+// =============================================================================
+// main() Program Entry
+// =============================================================================
+int main(int argc, char *argv[])
+{
+
+  int num=0;
+  std::fstream inputFile;
+  int rArray[1000];
+  char filePath[100]="inputFunction.txt";
+  readData(filePath, rArray, num);
+
+
+  char inputPPMFile[100];
+  cout<<"Enter the ppm file to be manipulated\n";
+  cin>>inputPPMFile;
+  readPPMFile(inputPPMFile);
+  pixmapComputed = new unsigned char[width * height * 3];
+
+
+  int option;
+  cout<<"Enter options:\n1.Linear interpolation\n2.Cubic hermitian interpolation\n";
+  cin>>option;
+  switch(option)
+  {
+    case 1:
+      colorManipulate(rArray,num-1);
+      break;
+    case 2:
+      colorManipulate(rArray,num-1);
+      break;
+  }
+
+
+  generatePPMFile(option);
+
+
+  glutInit(&argc, argv);
+  glutInitWindowPosition(100, 100); // Where the window will display on-screen.
+  glutInitWindowSize(width, height);
+  glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+  glutCreateWindow("Homework Zero");
+  init();
+  glutReshapeFunc(windowResize);
+  glutDisplayFunc(windowDisplay);
+  glutMouseFunc(processMouse);
+  glutMainLoop();
+
+  return 0; //This line never gets reached. We use it because "main" is type int.
+}
+     
+
